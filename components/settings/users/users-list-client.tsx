fd90c735d5fe8864183f15633d1f174e6a8bcd7a
Williams Verde <wverde@sparkiq.io>
2025-10-27 15:24:04 -0400

    back-end-v2: user mmg

--- components/settings/users/users-list-client.tsx
+++ components/settings/users/users-list-client.tsx
@@ -12,6 +12,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { useToast } from "@/hooks/use-toast"
 import { getUsers, deleteUser, resendInvite, resetPassword } from "@/lib/actions/admin-users"
 import { CreateUserModal } from "./create-user-modal"
+import { CreateUserModalMinimal } from "./create-user-modal-minimal"
 import { EditUserModal } from "./edit-user-modal"
 import { RoleManagementModal } from "./role-management-modal"
 import type { AdminUser } from "@/lib/types/admin"
@@ -394,7 +395,8 @@ export function UsersListClient() {
       </Card>
 
       {/* Modals */}
-      <CreateUserModal open={showCreateModal} onOpenChange={setShowCreateModal} onSuccess={loadUsers} />
+      {/* Testing with minimal react-hook-form to isolate the issue */}
+      <CreateUserModalMinimal open={showCreateModal} onOpenChange={setShowCreateModal} onSuccess={loadUsers} />
 
       <EditUserModal open={showEditModal} onOpenChange={setShowEditModal} onSuccess={loadUsers} user={selectedUser} />
 
