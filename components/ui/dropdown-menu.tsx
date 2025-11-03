6896caa111b4956d680bfd2b308756edcb0f03ba
Williams Verde <wverde@sparkiq.io>
2025-11-02 19:38:50 -0500

    revert: remove redundant style prop merge

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -39,18 +39,13 @@ function DropdownMenuContent({
   return (
     <DropdownMenuPrimitive.Portal>
       <DropdownMenuPrimitive.Content
-        {...props}
         data-slot="dropdown-menu-content"
         sideOffset={sideOffset}
         className={cn(
           'bg-popover text-popover-foreground pointer-events-auto data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 !z-[10000] max-h-[var(--radix-dropdown-menu-content-available-height)] min-w-[8rem] origin-[var(--radix-dropdown-menu-content-transform-origin)] overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md',
           className,
         )}
-        style={{
-          ...(props as any).style,
-          position: 'relative',
-          display: 'block',
-        }}
+        {...props}
       />
     </DropdownMenuPrimitive.Portal>
   )
