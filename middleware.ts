682d427697da746b406ba0a2273a9e73f5957e27
Williams Verde <wverde@sparkiq.io>
2025-11-12 23:15:06 -0500

    wv: user invite production

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { NextResponse, type NextRequest } from "next/server"
 const PUBLIC_PATHS = [
   "/sign-in",
   "/auth/callback",
+  "/auth/set-password",
   "/q",
   "/public/itinerary",
   "/action",
